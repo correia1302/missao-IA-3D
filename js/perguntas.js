420793e81e8c0e1f92a68a51439b9d9cd8fcd3f4
correia1302 <161593384+correia1302@users.noreply.github.com>
2024-08-21 12:18:38 -0300

    .

---
+++ js/perguntas.js
@@ -0,0 +1,61 @@
+const perguntas = [
+    {
+        enunciado: "você apoia o racismo?",
+        alternativas: [
+            {
+                texto: "sim eu apoio o movimento racista ",
+                afirmacao: [
+                    "você realmente já ouviu  falar o que é o racismo?  A sua história e quantas milhares de pessoa foram prejudicadas e mortas? ",
+                    "afirmacao 2"
+                    ]
+            },
+            {
+                texto: "não, sou contra o movimento racista ",
+                afirmacao: [
+                    "que legal! concordamos que o racismo não é uma coisa boa ",
+                    "afirmacao 2"
+                    ]
+            }           
+            
+        ]
+    },
+    {
+        enunciado: "principal dano criado pelo rascimo",
+        alternativas: [
+            {
+                texto:"fortalece a altoestima das vitimas",
+                afirmacao: [
+                    "enfraquece a altoestisma, perpetuando sentimentos de inferioridade",
+                    "afirmacao 2"
+                    ]
+            },
+            {
+                texto: "Contribui para exclusãõ social e marginalização de grupos minoridarios",
+                afirmacao: [
+                    "reforça a desigualdade e esteriótipos discriminatórios",
+                    "afirmacao 2"
+                    ]
+            }
+        ]
+    },
+    {
+        enunciado: "Pergunta 3",
+        alternativas: [
+            {
+                texto:"Alternativa 1 da pergunta 3",
+                afirmacao: [
+                    "afirmacao 1",
+                    "afirmacao 2"
+                    ]
+            },
+            {
+                texto:"Alternativa 2 da pergunta 3",
+                afirmacao: [
+                    "afirmacao 1",
+                    "afirmacao 2"
+                    ]
+            }
+            
+        ]
+    },
+];
