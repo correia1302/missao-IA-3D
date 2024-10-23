cce7d5e3366ca69ffdda6846256c3f73d3779ed5
correia1302 <161593384+correia1302@users.noreply.github.com>
2024-10-23 08:14:41 -0300

    Update aleatorio.js

--- js/aleatorio.js
+++ js/aleatorio.js
@@ -1,8 +1,8 @@
-const nomes = ["josafa", "alef", "alifer, "antonia", "neuza", "terezinha"];
+const nomes = ["Juca", "Margarida", "Alfredo", "Deolane", "Catra", "Pipokinha"];
 
 export function aleatorio (lista){
-  const posicao = Math.floor(Math.random()* Lista.Lenght);
-  return lista[posicao]; 
+    const posicao = Math.floor(Math.random()* lista.length);
+    return lista[posicao];
 }
 
 export const nome = aleatorio(nomes)
